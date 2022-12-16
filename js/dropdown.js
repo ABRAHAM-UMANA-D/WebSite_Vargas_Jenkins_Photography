058f4ac737e1e9c8ac82ca6eed6f992dbf270690
Abraham-Umana-Delgado <abraham.umana.delgado@est.una.ac.cr>
2022-12-15 21:42:22 -0600

    Update #1 (Abraham) - 15/12/2022

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,7 +2,6 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown if the user clicks outside of it
   window.onclick = function(e) {
     if (!e.target.matches('.nav_link')) {
     var myDropdown = document.getElementById("myDropdown");
