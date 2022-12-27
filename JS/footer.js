9729688f1f7ce2fe6a21b6ec914f3be834ba8538
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2022-12-27 16:25:46 -0600

    Updated 27/12/2022

---
+++ JS/footer.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $('.footerContainer').load('../html/footer.html');
+  });
--- JS/navbar.js
+++ JS/navbar.js
@@ -1,13 +1,3 @@
-(function () {
-    const openButton = document.querySelector('.nav_menu');
-    const menu = document.querySelector('.navbar-nav');
-    const closeMenu = document.querySelector('.nav_close');
-
-    openButton.addEventListener('click', () => {
-        menu.classList.add('navbar-nav--show');
-    });
-
-    closeMenu.addEventListener('click', () => {
-        menu.classList.remove('navbar-nav--show');
-    });
-})();
+$(document).ready(function () {
+    $('.menuContainer').load('../html/navbar.html');
+  });
