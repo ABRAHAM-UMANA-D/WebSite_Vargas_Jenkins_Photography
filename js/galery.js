ab7b8f0493915b1b5ddc57d0ec2613ef15ed969d
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2022-12-28 19:53:08 -0600

    Updated #1 Jeimmy 28/12/2022
    Adapt to all browsers the galery and add the color to set

--- js/galery.js
+++ js/galery.js
@@ -70,8 +70,6 @@ window.onload = () => {
 
 }
 
-
-
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -82,14 +80,15 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slide-container");
-  var dots = document.getElementsByClassName("demo");
+  var btn = document.getElementsByClassName("options");
   if (n > x.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = x.length }
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-red", "");
+  for (i = 0; i < btn.length; i++) {
+    btn[i].className = btn[i].className.replace(" options-color", "");
   }
-  x[slideIndex - 1].style.display = "flex";
+  x[slideIndex - 1].style.display = "grid";
+  btn[slideIndex-1].className += " options-color";
 }
