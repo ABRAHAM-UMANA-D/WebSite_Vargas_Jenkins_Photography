ab9f02396058b7707e66dbabc68d04281919cda2
Abraham-Umana-Delgado <abraham.umana.delgado@est.una.ac.cr>
2023-01-10 14:24:35 -0600

    Update  #1 Abraham - (10/01/2023)

--- js/testimony.js
+++ js/testimony.js
@@ -1,6 +1,6 @@
 (function () {
 
-    const elements = [...document.querySelectorAll('.testimony_body')];
+    const elements = [...document.querySelectorAll('.testimony-body')];
     const avanzar = document.querySelector('#next');
     const retroceder = document.querySelector('#before');
     let valor;
@@ -15,7 +15,7 @@
 
     const cambio_de_posicion = (add) => {
 
-        const testimonioActual = document.querySelector('.testimony_body--show').dataset.id;
+        const testimonioActual = document.querySelector('.testimony-body--show').dataset.id;
 
         /*Una constante que va a seleccionar especificamente al testimonio de la seccion 1 el cual es
         el testimony_body--show y va a obtener el dataset de ese testimonio de la seccion uno, que es el ID = 1*/
@@ -26,14 +26,14 @@
 
         //elements.lengt = Maximo de elementos en el slider
 
-        elements[Number(testimonioActual) - 1].classList.remove('testimony_body--show');
+        elements[Number(testimonioActual) - 1].classList.remove('testimony-body--show');
         if (valor === elements.length + 1 || valor === 0) {
             valor = valor === 0 ? elements.length : 1;
         }
         /* Si valor es igual al maximo de elementos del slider y yo intento sumar uno mas entonces que se me devuelva 
         a la posicion 1 del slider, o si valor es igual al primer elemento y yo resto 1, que se pase al ultimo elemento del slider */
 
-        elements[valor - 1].classList.add('testimony_body--show');
+        elements[valor - 1].classList.add('testimony-body--show');
 
         //Accediendo al primer elemento del slider, -1 porque en programacion los algoritmos cuentan desde 0
         //La posicion 1 = 0, la 2 = 1, 3 = 2, por eso hay que hacer ese descuento
