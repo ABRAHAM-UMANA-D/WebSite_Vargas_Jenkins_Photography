c49778ac9c10c1cfc4e8d2d603c0e5716d95d4d7
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-01-26 17:38:30 -0600

    Package

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,12 +1,21 @@
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("dropdownGalery").classList.toggle("show");
+}
+
+function dropdownPackages(){
+  document.getElementById("dropdownPackage").classList.toggle("show");
 }
 
 window.onclick = function (e) {
   if (!e.target.matches('.nav_link')) {
-    var myDropdown = document.getElementById("myDropdown");
+    var myDropdown = document.getElementById("dropdownGalery");
+    var dropdownPackage = document.getElementById("dropdownPackage");
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
+    }
+
+    if (dropdownPackage.classList.contains('show')) {
+      dropdownPackage.classList.remove('show');
     }
   }
 }
