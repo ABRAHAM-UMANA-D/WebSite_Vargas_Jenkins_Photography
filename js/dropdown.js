a5d9a935c1b582475163ba0eca208f906cbbd870
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-01-26 20:00:42 -0600

    Updated

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,20 +2,25 @@ function myFunction() {
   document.getElementById("dropdownGalery").classList.toggle("show");
 }
 
-function dropdownPackages(){
-  document.getElementById("dropdownPackage").classList.toggle("show");
-}
-
-window.onclick = function (e) {
+window.addEventListener("click", function (e) {
   if (!e.target.matches('.nav_link')) {
     var myDropdown = document.getElementById("dropdownGalery");
-    var dropdownPackage = document.getElementById("dropdownPackage");
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
     }
+  }
+});
 
-    if (dropdownPackage.classList.contains('show')) {
-      dropdownPackage.classList.remove('show');
+function dropdownPackages(){
+  document.getElementById("dropdownPackage").classList.toggle("show-package");
+}
+
+window.addEventListener("click", function (e) {
+  if (!e.target.matches('.nav_link')) {
+    var dropdownPackage = document.getElementById("dropdownPackage");
+    if (dropdownPackage.classList.contains('show-package')) {
+      dropdownPackage.classList.remove('show-package');
     }
   }
-}
+});
+
