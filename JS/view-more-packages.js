4d774c11e96fcc0721c39367f08d48fb3ace5434
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-01-30 16:57:56 -0600

    Packages

--- JS/view-more-packages.js
+++ JS/view-more-packages.js
@@ -1,11 +1,16 @@
 var benefits_premium = document.querySelectorAll('.benefits');
 var benefits_platinum = document.querySelectorAll('.benefits-platinum');
+var benefits_golden = document.querySelectorAll('.benefits-golden');
+
 var btn_load = document.querySelector('.btn-view-more');
+
 var btn_load_first = document.getElementById("btn-one");
 var btn_load_two = document.getElementById("btn-two");
 var btn_load_three = document.getElementById("btn-three");
+
 var benefitCurrent = 3
 var planinumCurrent = 3
+var goldenCurrent = 3
 
 function viewMore() {
     for (var i = benefitCurrent; i < benefitCurrent + 3; i++) {
@@ -32,6 +37,20 @@ function viewMorePlatinum() {
     planinumCurrent += 3;
     if(planinumCurrent>=benefits_platinum.length){
         btn_load_two.style.display = 'none'
+    }
+}
+
+function viewMoreGolden() {
+    for (var i = goldenCurrent; i < goldenCurrent + 3; i++) {
+        console.log(i);
+        if (benefits_golden[i]) {
+            console.log(benefits_golden[i]);
+            benefits_golden[i].style.display = 'block';
+        }   
+    }
+    goldenCurrent += 3;
+    if(goldenCurrent>=benefits_golden.length){
+        btn_load_three.style.display = 'none'
     }
 }
 
