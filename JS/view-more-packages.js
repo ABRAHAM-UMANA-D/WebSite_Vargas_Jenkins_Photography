29246e70551019ce12973448921f6dba7f2bb761
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-01-29 21:53:42 -0600

    updated

--- JS/view-more-packages.js
+++ JS/view-more-packages.js
@@ -1,16 +1,16 @@
-var benefits_package = document.querySelectorAll('.wrapper-gallery');
-var btn_load = document.querySelector('.btn-load-more');
-var benefitCurrent = 1
+var benefits_package = document.querySelectorAll('.benefits');
+var btn_load = document.querySelector('.btn-view-more');
+var benefitCurrent = 3
 
-function showMore() {
-    for (var i = benefitCurrent; i < benefitCurrent + 1; i++) {
+function viewMore() {
+    for (var i = benefitCurrent; i < benefitCurrent + 3; i++) {
         console.log(i);
         if (benefits_package[i]) {
             console.log(benefits_package[i]);
             benefits_package[i].style.display = 'block';
         }   
     }
-    benefitCurrent += 1;
+    benefitCurrent += 3;
     if(benefitCurrent>=benefits_package.length){
         btn_load.style.display = 'none'
     }
