61c5fc4b7004dda249ed794c76c7956b5773eae8
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-01-31 23:14:34 -0600

    Packages

--- JS/view-more-packages.js
+++ JS/view-more-packages.js
@@ -1,56 +1,54 @@
-var benefits_premium = document.querySelectorAll('.benefits');
-var benefits_platinum = document.querySelectorAll('.benefits-platinum');
-var benefits_golden = document.querySelectorAll('.benefits-golden');
+var benefits_one = document.querySelectorAll('.benefit-one');
+var benefits_two = document.querySelectorAll('.benefit-two');
+var benefits_three = document.querySelectorAll('.benefit-three');
 
-var btn_load = document.querySelector('.btn-view-more');
+var btn_contact = document.querySelector('.btn-contact-pk');
 
 var btn_load_first = document.getElementById("btn-one");
 var btn_load_two = document.getElementById("btn-two");
 var btn_load_three = document.getElementById("btn-three");
 
-var benefitCurrent = 3
-var planinumCurrent = 3
-var goldenCurrent = 3
+var benefitCurrent = 1
+var benefitSecondCurrent = 1
+var benefitsThreeCurrent = 1
 
 function viewMore() {
-    for (var i = benefitCurrent; i < benefitCurrent + 3; i++) {
+    for (var i = benefitCurrent; i < benefits_one.length; i++) {
         console.log(i);
-        if (benefits_premium[i]) {
-            console.log(benefits_premium[i]);
-            benefits_premium[i].style.display = 'block';
-        }   
-    }
-    benefitCurrent += 3;
-    if(benefitCurrent>=benefits_premium.length){
+        if (benefits_one[i]) {
+            console.log(benefits_one[i]);
+            benefits_one[i].style.display = 'block';
+        }
         btn_load_first.style.display = 'none'
+        btn_contact.style.display = 'block'
     }
+    benefitCurrent += 1;
 }
 
-function viewMorePlatinum() {
-    for (var i = planinumCurrent; i < planinumCurrent + 3; i++) {
+function viewMoreTwo() {
+    for (var i = benefitSecondCurrent; i < benefits_two.length; i++) {
         console.log(i);
-        if (benefits_platinum[i]) {
-            console.log(benefits_platinum[i]);
-            benefits_platinum[i].style.display = 'block';
-        }   
+        if (benefits_two[i]) {
+            console.log(benefits_two[i]);
+            benefits_two[i].style.display = 'block';
+        }
+        btn_load_two.style.display = 'none';
+        btn_contact.style.display = 'block'
     }
-    planinumCurrent += 3;
-    if(planinumCurrent>=benefits_platinum.length){
-        btn_load_two.style.display = 'none'
-    }
+
+    benefitSecondCurrent += 1;
 }
 
-function viewMoreGolden() {
-    for (var i = goldenCurrent; i < goldenCurrent + 3; i++) {
+function viewMoreThree() {
+    for (var i = benefitsThreeCurrent; i < benefits_three.length; i++) {
         console.log(i);
-        if (benefits_golden[i]) {
-            console.log(benefits_golden[i]);
-            benefits_golden[i].style.display = 'block';
-        }   
-    }
-    goldenCurrent += 3;
-    if(goldenCurrent>=benefits_golden.length){
+        if (benefits_three[i]) {
+            console.log(benefits_three[i]);
+            benefits_three[i].style.display = 'block';
+        }
         btn_load_three.style.display = 'none'
+        btn_contact.style.display = 'block'
     }
+    benefitsThreeCurrent += 2;
 }
 
