c3260c16373460ed6eca0adefdb177fe56a358ef
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-02-13 21:20:10 -0600

    Menu

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,20 @@
-(function () {
+const toggleButton = document.getElementById('nav_menu')
+const navWrapper = document.getElementById('nav')
+
+toggleButton.addEventListener('click',() => {
+  toggleButton.classList.toggle('close')
+  navWrapper.classList.toggle('show')
+})
+
+navWrapper.addEventListener('click',e => {
+  if(e.target.id === 'nav'){
+    navWrapper.classList.remove('show')
+    toggleButton.classList.remove('close')
+  }
+})
+
+
+/* (function () {
     const openButton = document.querySelector('.nav_menu');
     const menu = document.querySelector('.navbar-nav');
     const closeMenu = document.querySelector('.nav_close');
@@ -11,4 +27,4 @@
         menu.classList.remove('navbar-nav--show');
     });
     
-})();
+})(); */
