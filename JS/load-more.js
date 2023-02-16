16b79e98663c0c2384f1ae761e6afb6d51de7f8c
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-02-15 22:23:09 -0600

    Updated

--- JS/load-more.js
+++ JS/load-more.js
@@ -1,17 +1,17 @@
-var testimonies = document.querySelectorAll('.wrapper-gallery');
+var galleries = document.querySelectorAll('.wrapper-gallery');
 var btn_view_more = document.querySelector('.btn-load-more');
-var testimony_current = 1
+var gallery_current = 1
 
 function showMore() {
-    for (var i = testimony_current; i < testimony_current + 1; i++) {
+    for (var i = gallery_current; i < gallery_current + 1; i++) {
         console.log(i);
-        if (testimonies[i]) {
-            console.log(testimonies[i]);
-            testimonies[i].style.display = 'block';
+        if (galleries[i]) {
+            console.log(galleries[i]);
+            galleries[i].style.display = 'block';
         }   
     }
-    testimony_current += 1;
-    if(testimony_current>=testimonies.length){
-        btn_view_more.style.display = 'block'
+    gallery_current += 1;
+    if(gallery_current>=galleries.length){
+        btn_view_more.style.display = 'none'
     }
 }
