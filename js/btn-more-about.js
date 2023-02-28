288a9f1978b0439ab1b0ad4e8ce1e32456642f9c
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-02-28 14:44:52 -0600

    Ver más

---
+++ js/btn-more-about.js
@@ -0,0 +1,14 @@
+const buttons = document.querySelectorAll('.btn-hidden');
+const textElements = document.querySelectorAll('.hidden-text');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const textElement = textElements[index];
+    textElement.classList.toggle('show-text');
+    if(textElement.classList.contains('show-text')){
+        button.innerHTML = 'Leer menos';
+    }else{
+        button.innerHTML = 'Leer más';
+    }
+  });
+});
