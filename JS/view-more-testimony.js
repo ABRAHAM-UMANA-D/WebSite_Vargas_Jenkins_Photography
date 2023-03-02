7a3c9d6691a7be017b12535f7ed8f2880da64abc
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-03-02 17:39:24 -0600

    Updated

--- JS/view-more-testimony.js
+++ JS/view-more-testimony.js
@@ -1,16 +1,18 @@
-var testimonies = document.querySelectorAll('.wrapper-testimony');
-var btn_view_more = document.querySelector('.testimony-view-more');
+var testimonies = document.querySelectorAll('.hide-testimony');
+var btn_view_more = document.getElementById('btn-testimony-view');
 
-var testimony_current = 1
+var testimony_current = 3
+
+btn_view_more.addEventListener('click', showMore);
 
 function showMore() {
-    for (var i = testimony_current; i < testimony_current + 1; i++) {
+    for (var i = testimony_current; i < testimony_current + 3; i++) {
         if (testimonies[i]) {
             testimonies[i].style.display = 'block';
         }
     }
-    testimony_current += 1;
+    testimony_current += 3;
     if (testimony_current >= testimonies.length) {
-        btn_view_more.style.display = "none"
+        btn_view_more.style.display = 'none';
     }
 }
