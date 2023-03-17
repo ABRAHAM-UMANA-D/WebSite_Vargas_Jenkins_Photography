eb7459dc2a377b0103dd92fbbb6c41586928efb9
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-03-16 22:03:00 -0600

    Updated

--- JS/view-more-packages.js
+++ JS/view-more-packages.js
@@ -1,47 +1,14 @@
-var benefits_one = document.querySelectorAll('.benefit-one');
-var benefits_two = document.querySelectorAll('.benefit-two');
-var benefits_three = document.querySelectorAll('.benefit-three');
+const btn_benefits = document.querySelectorAll('.btn-hide-benefits');
+const hidden_benefits = document.querySelectorAll('.hidden-benefits');
 
-var btn_load_first = document.getElementById("btn-one");
-var btn_load_two = document.getElementById("btn-two");
-var btn_load_three = document.getElementById("btn-three");
-
-var benefitCurrent = 1
-var benefitSecondCurrent = 1
-var benefitsThreeCurrent = 1
-
-function viewMore() {
-    for (var i = benefitCurrent; i < benefits_one.length; i++) {
-        console.log(i);
-        if (benefits_one[i]) {
-            console.log(benefits_one[i]);
-            benefits_one[i].style.display = 'block';
-        }
-        btn_load_first.style.display = 'none';
-    }
-    benefitCurrent += 1;
-}
-
-function viewMoreTwo() {
-    for (var i = benefitSecondCurrent; i < benefits_two.length; i++) {
-        console.log(i);
-        if (benefits_two[i]) {
-            console.log(benefits_two[i]);
-            benefits_two[i].style.display = 'block';
-        }
-        btn_load_two.style.display = 'none';
-    }
-    benefitSecondCurrent += 1;
-}
-
-function viewMoreThree() {
-    for (var i = benefitsThreeCurrent; i < benefits_three.length; i++) {
-        console.log(i);
-        if (benefits_three[i]) {
-            console.log(benefits_three[i]);
-            benefits_three[i].style.display = 'block';
-        }
-        btn_load_three.style.display = 'none'
-    }
-    benefitsThreeCurrent += 2;
-}
+btn_benefits.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      const textElement = hidden_benefits[index];
+      textElement.classList.toggle('show-benefits');
+      if (textElement.classList.contains('show-benefits')) {
+        button.innerHTML = 'Ver menos';
+      } else {
+        button.innerHTML = 'Ver más detalles';
+      }
+    });
+  });
